6b7d5543b4bd969b522a13039b9f2c79d6e941aa
agustinc08 <loned@live.com.ar>
2023-11-22 20:30:59 -0300

    asd

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ app.use('/adopcion', new RouterAdopcion().start())
 app.use('/animal', new RouterAnimal().start())
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.listen(port, () => {
     console.log(`Servidor escuchando en https://bepawsome-e858795261d3.herokuapp.com:${port}`);
